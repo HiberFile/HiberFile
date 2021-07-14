890e28d38846a09545d96164ab2f5e879aa1a8ad
Vergasta Paiva <VERGASTA_B@swatchgroup.net>
2021-07-14 08:34:38 +0200

    feat: flags removed in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="my-12 text-grey-500 font-semibold">
 		<nuxt-link :to="localePath('/mentions')" class="text-xs"
-			><u>{{ $t('legal_notice') }}</u> | © 2021 HiberFile Team - 🇨🇭🇫🇷 | v2021.1</nuxt-link
+			><u>{{ $t('legal_notice') }}</u> | © 2021 HiberFile Team | v2021.1</nuxt-link
 		>
 	</footer>
 </template>
