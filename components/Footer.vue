212b8178bf80b9eb61ff06b2d6487f9414f6c992
arthur-fontaine <0arthur.fontaine@gmail.com>
2021-07-21 14:38:34 +0200

    feat: add a button to go to the blog

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,16 @@
 <template>
-	<footer class="my-12 text-grey-500 font-semibold">
+	<footer
+		class="my-12 text-grey-500 font-semibold flex flex-row justify-between"
+	>
 		<nuxt-link :to="localePath('/mentions')" class="text-xs"
-			><u>{{ $t('legal_notice') }}</u> | © 2021 HiberFile Team | v2021.1</nuxt-link
+			><u>{{ $t('legal_notice') }}</u> | © 2021 HiberFile Team |
+			v2021.1</nuxt-link
+		>
+		<a
+			target="_blank"
+			href="https://blog.hiberfile.com/"
+			class="text-xs underline"
+			>Blog</a
 		>
 	</footer>
 </template>
