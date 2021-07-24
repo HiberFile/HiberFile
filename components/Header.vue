6dd0412320822d924a5c587d33e009c7774bddbd
Bruno Paiva <ppaiva11144@gmail.com>
2021-07-24 18:13:21 +0200

    fix: header bug

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
 	<header class="h-content flex justify-between items-start flex-shrink-0">
 		<div class="inline-block" @click="onclicklogo">
 			<Logo class="hiberfile-logo cursor-pointer" />
-			<p class="font-light mt-8" style="width: 14rem">
+			<p class="font-light mt-8" style="width: 14rem" v-if="!mobile">
 				{{ $t('share_file_header') }}
 			</p>
 		</div>
