ba6cb43ae232d00f6ab74b83cdfdc99637d1423e
arthur-fontaine <0arthur.fontaine@gmail.com>
2021-10-13 14:58:54 +0200

    feat(components): add error and type to the Input component

--- components/Input.vue
+++ components/Input.vue
@@ -1,14 +1,30 @@
 <template>
-	<div class="w-full border-blue-700 border-b">
-		<input
-			type="text"
-			class="text-base font-light text-blue-700 outline-none w-full"
-			@change="handleInput"
-			:value="value"
-			:placeholder="placeholder"
-			:aria-placeholder="placeholder"
-		/>
-	</div>
+  <div
+    class="w-full"
+    :class="{
+      'border-blue-700': error === undefined || error === true,
+      'border-red-700': error === false,
+      'border-b-0': pad,
+      'border-b': !pad
+    }"
+  >
+    <input
+      v-model="inputValue"
+      :type="_type"
+      class="text-base font-light outline-none w-full"
+      :class="{
+        'p-2': pad,
+        'rounded-lg': pad,
+        'text-blue-700': !pad || error === false || error === undefined,
+        'text-red-700': pad && error === true
+      }"
+      :value="value"
+      :autocomplete="autocomplete"
+      :placeholder="placeholder"
+      :aria-placeholder="placeholder"
+      @input="handleInput"
+    />
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,24 +32,43 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 
 @Component
 export default class Input extends Vue {
-	@Prop()
-	readonly placeholder: string | undefined;
+  @Prop()
+  readonly placeholder: string | undefined;
 
-	@Prop()
-	readonly value: string | undefined;
+  @Prop()
+  readonly value: string | undefined;
 
-	get inputValue() {
-		return (this.$el.querySelector('input') as HTMLInputElement).value;
-	}
+  @Prop()
+  readonly type: 'text' | 'password' | 'email' | undefined;
 
-	handleInput() {
-		this.$emit('input', this.inputValue);
-	}
+  @Prop()
+  readonly autocomplete: string | undefined;
+
+  @Prop()
+  readonly error: boolean | undefined;
+
+  @Prop()
+  readonly pad: boolean | undefined;
+
+  private _type: string | undefined;
+  private _pad: boolean | undefined;
+
+  inputValue: string = '';
+
+  beforeMount() {
+    this._type = this.type ?? 'text';
+    this._pad = this.pad ?? false;
+    if (this.value) this.inputValue = this.value;
+  }
+
+  handleInput() {
+    this.$emit('input', this.inputValue);
+  }
 }
 </script>
 
 <style scoped>
 input::placeholder {
-	@apply text-blue-700 text-opacity-50;
+  @apply text-blue-700 text-opacity-50;
 }
 </style>
