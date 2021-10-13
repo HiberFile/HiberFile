8de6432cf0c38e0a9bc10661a78a3fdf32a7ec7f
arthur-fontaine <0arthur.fontaine@gmail.com>
2021-10-13 14:58:00 +0200

    feat: use the maincard as a flexbox

--- components/MainCard.vue
+++ components/MainCard.vue
@@ -1,20 +1,20 @@
 <template>
-	<main
-		class="max-w-full md:max-w-none"
-		style="width: 28rem; min-height: 20rem"
-	>
-		<div
-			class="w-full inline-block rounded-3xl shadow-xl h-full p-4 md:max-h-160 md:min-h-144 bg-white"
-		>
-			<slot></slot>
-		</div>
-	</main>
+  <main
+    class="max-w-full md:max-w-none flex flex-col justify-center"
+    style="width: 28rem; min-height: 20rem"
+  >
+    <div
+      class="w-full inline-block rounded-3xl shadow-xl h-full p-4 md:max-h-160 md:min-h-144 bg-white"
+    >
+      <slot></slot>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-main {
-	@media (min-width: 768px) {
-		min-width: fit-content;
-	}
+@media (min-width: 768px) {
+  main {
+    min-width: fit-content;
+  }
 }
 </style>
