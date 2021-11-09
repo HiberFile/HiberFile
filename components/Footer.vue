9ee81d8ee7a26764561af834c31d97761a19c6bf
Arthur Fontaine <0arthur.fontaine@gmail.com>
2021-11-09 21:37:43 +0100

    feat: update the version number

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,24 +1,24 @@
 <template>
-	<footer class="text-white flex flex-row justify-end flex-shrink-0">
-		<div class="text-right font-light text-xs">
-			<a href="https://blog.hiberfile.com/" class="underline">Blog</a>
-			<div class="block">
-				<a
-					:href="
-						commitSha !== undefined
-							? `https://github.com/hiberfile/hiberfile/commit/${commitSha}`
-							: ''
-					"
-					target="_blank"
-					>v2021.2</a
-				>
-				- © 2021 HiberFile Team -
-				<nuxt-link :to="localePath('/mentions')">
-					<u>{{ $t('legal_notice') }}</u></nuxt-link
-				>
-			</div>
-		</div>
-	</footer>
+  <footer class="text-white flex flex-row justify-end flex-shrink-0">
+    <div class="text-right font-light text-xs">
+      <a href="https://blog.hiberfile.com/" class="underline">Blog</a>
+      <div class="block">
+        <a
+          :href="
+            commitSha !== undefined
+              ? `https://github.com/hiberfile/hiberfile/commit/${commitSha}`
+              : ''
+          "
+          target="_blank"
+          >v2021.3</a
+        >
+        - © 2021 HiberFile Team -
+        <nuxt-link :to="localePath('/mentions')">
+          <u>{{ $t('legal_notice') }}</u></nuxt-link
+        >
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
@@ -26,8 +26,8 @@ import { Component, Vue } from 'nuxt-property-decorator';
 
 @Component
 export default class Footer extends Vue {
-	get commitSha() {
-		return process.env.NUXT_ENV_CURRENT_GIT_SHA;
-	}
+  get commitSha() {
+    return process.env.NUXT_ENV_CURRENT_GIT_SHA;
+  }
 }
 </script>
