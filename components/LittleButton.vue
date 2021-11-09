3cee002a6e3b111d14158ecfbceb03e1165fea90
Arthur Fontaine <0arthur.fontaine@gmail.com>
2021-11-09 20:39:05 +0100

    feat(components): add color and size properties to LittleButton

--- components/LittleButton.vue
+++ components/LittleButton.vue
@@ -1,11 +1,11 @@
 <template>
-	<p
-		class="outline-none bg-opacity-0 bg-white underline cursor-pointer font-normal text-base"
-		style="width: max-content"
-		:class="`text-${_color}`"
-	>
-		{{ value }}
-	</p>
+  <p
+    class="outline-none bg-opacity-0 bg-white underline cursor-pointer font-normal"
+    style="width: max-content"
+    :class="`text-${_color} text-${_size}`"
+  >
+    {{ value }}
+  </p>
 </template>
 
 <script lang="ts">
@@ -13,18 +13,24 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 
 @Component
 export default class Button extends Vue {
-	@Prop({
-		required: true
-	})
-	readonly value: string | undefined;
+  @Prop({
+    required: true
+  })
+  readonly value: string | undefined;
 
-	@Prop()
-	readonly color: string | undefined;
+  @Prop()
+  readonly color: string | undefined;
 
-	private _color: string | undefined;
+  private _color: string | undefined;
 
-	beforeMount() {
-		this._color = this.color ?? 'blue-700';
-	}
+  @Prop()
+  readonly size: string | undefined;
+
+  private _size: string | undefined;
+
+  beforeMount() {
+    this._color = this.color ?? 'blue-700';
+    this._size = this.size ?? 'base';
+  }
 }
 </script>
