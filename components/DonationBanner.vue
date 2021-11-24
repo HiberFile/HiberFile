731a5bc9ba275ad5db672e57c955f2913ddb2172
Arthur Fontaine <0arthur.fontaine@gmail.com>
2021-11-24 10:17:35 +0100

    feat: add a donation banner

---
+++ components/DonationBanner.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-show="opened" class="bg-white flex justify-center p-1 items-center">
+    <i18n
+      class="text-blue-700 bg w-auto text-base font-normal mr-6"
+      path="donation_banner"
+      tag="p"
+    >
+      <template #click_here>
+        <a
+          class="underline"
+          href="https://fr.liberapay.com/hiberfile/"
+          target="_blank"
+          >{{ $t('click_here') }}</a
+        >
+      </template>
+    </i18n>
+    <Cross @click.native="close" class="h-2 w-2 cursor-pointer" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+export default class Index extends Vue {
+  opened = false;
+
+  beforeMount() {
+    if (new Date().getDate() === 5) this.opened = true;
+  }
+
+  close() {
+    this.opened = false;
+  }
+}
+</script>
