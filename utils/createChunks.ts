8246ecbebd734e3a51dab7e6fe0bb44c1a9a62e7
Qexat <contact@qexat.com>
2021-12-12 18:16:27 +0100

    same commit as before

--- utils/createChunks.ts
+++ utils/createChunks.ts
@@ -1,11 +1,11 @@
 export default (file: File, chunkSize: number) => {
-	let startPointer = 0;
-	let endPointer = file.size;
-	let chunks = [];
-	while (startPointer < endPointer) {
-		let newStartPointer = startPointer + chunkSize;
-		chunks.push(file.slice(startPointer, newStartPointer));
-		startPointer = newStartPointer;
-	}
-	return chunks;
+  let startPointer = 0;
+  const endPointer = file.size;
+  const chunks = [];
+  while (startPointer < endPointer) {
+    const newStartPointer = startPointer + chunkSize;
+    chunks.push(file.slice(startPointer, newStartPointer));
+    startPointer = newStartPointer;
+  }
+  return chunks;
 };
