47b0b03ea8da8e6c3224f9ee887d3280610ff8bd
arthur-fontaine <0arthur.fontaine@gmail.com>
2021-12-30 00:56:21 +0100

    feat: adding an util to create the S3MultipartUpload

--- utils/s3.ts
+++ utils/s3.ts
@@ -1,9 +1,15 @@
-import {S3Client} from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  CreateMultipartUploadCommand,
+  UploadPartCommand,
+  CreateMultipartUploadCommandInput
+} from "@aws-sdk/client-s3";
+import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
 
 require('dotenv').config();
 
-if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-    throw new Error('AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables must be set');
+if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.AWS_BUCKET_NAME) {
+  throw new Error('AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_BUCKET_NAME environment variables must be set');
 }
 
 export const s3Client = new S3Client({
@@ -14,3 +20,30 @@ export const s3Client = new S3Client({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 })
+
+export const createS3MultipartUpload = async (chunkNumber: number, customCreateMultipartUploadOptions: Partial<CreateMultipartUploadCommandInput> & { Key: string }) => {
+  const createMultipartUploadOptions: CreateMultipartUploadCommandInput = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    ...customCreateMultipartUploadOptions,
+  };
+
+  const createMultipartUploadCommand = new CreateMultipartUploadCommand(createMultipartUploadOptions);
+  const {UploadId} = await s3Client.send(createMultipartUploadCommand);
+
+  return await Promise.all(
+    Array.from(Array(chunkNumber).keys()).map(async (i) => {
+      const uploadPartCommand = new UploadPartCommand({
+        ...createMultipartUploadOptions,
+        UploadId,
+        PartNumber: i + 1,
+      });
+
+      const url = await getSignedUrl(s3Client, uploadPartCommand);
+
+      return {
+        url,
+        partNumber: i + 1,
+      };
+    })
+  );
+}
