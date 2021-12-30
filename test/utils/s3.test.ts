3d3dc74a9c83181ae76fc6802c02906c3f9a16d1
arthur-fontaine <0arthur.fontaine@gmail.com>
2021-12-30 01:32:23 +0100

    test: add a test for the expiration of presigned urls

--- test/utils/s3.test.ts
+++ test/utils/s3.test.ts
@@ -1,4 +1,5 @@
 import { S3Client } from "@aws-sdk/client-s3";
+import moment from "moment";
 import {createS3MultipartUpload, s3Client} from "~/utils/s3";
 
 describe('s3Client', () => {
@@ -10,10 +11,11 @@ describe('s3Client', () => {
 describe('createS3MultipartUpload', () => {
   it('should create a list of presigned urls', async () => {
     const chunkNumber = 10;
+    const expiration = moment().add(1, 'minute').toDate();
 
     const s3MultipartUpload = await createS3MultipartUpload(chunkNumber, {
       Key: 'test-key',
-      Expires: new Date(Date.now() + 1000),
+      Expires: expiration,
     });
 
     expect(s3MultipartUpload.uploadId).toBeDefined();
@@ -28,6 +30,11 @@ describe('createS3MultipartUpload', () => {
       expect(s3PartUpload.url).toContain('X-Amz-Credential');
       expect(s3PartUpload.url).toContain('X-Amz-Signature');
       expect(s3PartUpload.partNumber).toBe(i + 1);
+
+      const date = moment(s3PartUpload.url.match(/X-Amz-Date=(\d+T\d+Z)/)?.[1]);
+      const expiresAt = date.add(parseFloat(s3PartUpload.url.match(/X-Amz-Expires=(\d+)/)?.[1]  ?? '0'), 's');
+
+      expect(expiresAt.diff(expiration, 's')).toBeLessThanOrEqual(1);
     });
   });
 })
