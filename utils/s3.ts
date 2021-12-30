c32e0f4bb7d8ab10a50fe9de9e0ab1c5bcb7c802
arthur-fontaine <0arthur.fontaine@gmail.com>
2021-12-30 16:53:06 +0100

    feat: adding util to upload file on the S3

--- utils/s3.ts
+++ utils/s3.ts
@@ -2,10 +2,12 @@ import {
   S3Client,
   CreateMultipartUploadCommand,
   UploadPartCommand,
-  CreateMultipartUploadCommandInput
+  CreateMultipartUploadCommandInput,
 } from "@aws-sdk/client-s3";
 import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
+import queue from 'queue';
 import moment from "moment";
+import axios, {AxiosRequestConfig} from "axios";
 
 require('dotenv').config();
 
@@ -55,4 +57,38 @@ export const createS3MultipartUpload = async (chunkNumber: number, customCreateM
       })
     )
   };
+}
+
+export const uploadFileS3Multipart = (
+  chunks: Blob[],
+  s3MultipartUpload: Awaited<ReturnType<typeof createS3MultipartUpload>>,
+  options: Partial<AxiosRequestConfig & {concurrency: number}> = {}
+) => {
+  return new Promise<{parts: {partNumber: number, eTag: string}[]}>(((resolve, reject) => {
+    const {concurrency = 6, ...axiosOptions} = options;
+
+    const {parts} = s3MultipartUpload;
+
+    const queueUpload = queue({concurrency, autostart: true});
+    const uploadResults: {partNumber: number, eTag: string}[] = [];
+
+    queueUpload.push(...parts.map(({url, partNumber}, i) => async () => {
+      const axiosResponse = await axios.put(url, chunks[i], axiosOptions);
+
+      if (axiosResponse.status.toString()[0] !== '2') {
+        reject(axiosResponse);
+      } else {
+        uploadResults.push({
+          partNumber,
+          eTag: axiosResponse.headers.etag,
+        });
+      }
+    }));
+
+    queueUpload.on('end', () => {
+      resolve({
+        parts: uploadResults.sort((a, b) => a.partNumber - b.partNumber),
+      });
+    });
+  }))
 }
