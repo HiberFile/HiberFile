59bcfb80339fadf0553c9626a97813111e19891d
arthur-fontaine <0arthur.fontaine@gmail.com>
2022-01-14 21:53:58 +0100

    feat: adding util to generate id

---
+++ utils/generateId.ts
@@ -0,0 +1,10 @@
+export default (length: number): string => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let id = '';
+
+  for (let i = 0; i < length; i++) {
+    id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return id;
+};
