93e6e06ae3a10ac6ee93b79ab6e8ae1088f76129
arthur-fontaine <0arthur.fontaine@gmail.com>
2022-01-15 01:00:44 +0100

    test: adding test to write the files routes

---
+++ test/api/routes/files.routes.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import api from "~/api/api";
+
+describe("Files routes", () => {
+  it('should create a file and get it', async () => {
+    const postRes = await request(api.handler)
+      .post("/files")
+      .send({
+        name: "test.txt"
+      });
+
+    const postData = postRes.body.data;
+
+    expect(postRes.status).toBe(200);
+    expect(postData.file.name).toBe("test.txt");
+    expect(postData.file.id).toBeDefined();
+
+    const getRes = await request(api.handler)
+      .get(`/files/${postData.file.id}`);
+
+    const getData = getRes.body.data;
+
+    expect(getRes.status).toBe(200);
+    expect(getData.file.name).toBe("test.txt");
+    expect(getData.file.id).toBeDefined();
+    expect(getData.file.id).toBe(postData.file.id);
+  });
+});
